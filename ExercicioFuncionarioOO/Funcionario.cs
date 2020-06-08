c340eea1b6669e6994adbaa2708d97b0a1dad665
Felipe Schein <felipe_str@hotmail.com>
2020-06-08 14:14:40 -0300

    projeto de cadastro de funcionario e aumento salarial

---
+++ ExercicioFuncionarioOO/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFuncionarioOO
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * (porcentagem / 100);
+        }
+
+        
+
+    }
+}
---
+++ ExercicioFuncionarioOO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFuncionarioOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: "+f.Nome +", $ " + f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + f.Nome + ", $ " + f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
